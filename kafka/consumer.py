194fa9e66b58d024a9900e33de5764731e003989
Lindelani <112875069+Lindelani21@users.noreply.github.com>
2025-03-18 10:00:02 +0200

    Update consumer.py

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -2,10 +2,12 @@ from kafka import KafkaConsumer
 import json
 
 consumer = KafkaConsumer(
+    
     "weather_data",
     bootstrap_servers="localhost:9092",
     auto_offset_reset="earliest",
     value_deserializer=lambda v: json.loads(v.decode("utf-8"))
+    
 )
 
 
